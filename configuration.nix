7a50a81eeec4cea7df2518a3b5d7b2f775a8dc18
ahtlon <atlan@ahtlon.de>
2024-11-24 23:09:05 +0100

    init

---
+++ configuration.nix
@@ -0,0 +1,181 @@
+# Edit this configuration file to define what should be installed on
+# your system. Help is available in the configuration.nix(5) man page, on
+# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
+
+{pkgs, inputs, config, ... }:
+{
+  imports = [ ./hardware-configuration.nix ];
+
+  boot.kernelPackages = pkgs.linuxPackages;
+  boot.supportedFilesystems = [ "zfs" ];
+  boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
+
+  networking.hostId = "990ee145";
+  system.nixos.tags = [ "minimal" ];
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.systemd-boot.configurationLimit = 20;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.zfs.requestEncryptionCredentials = true;
+
+  hardware.cpu.intel.updateMicrocode = true;
+  
+  hardware.bluetooth.enable = true; # enables support for Bluetooth
+  hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
+  services.blueman.enable = true;
+  
+  hardware.graphics.enable = true;
+  services.xserver.videoDrivers = [ "nvidia" ];
+  hardware.nvidia.open = true;
+
+  networking.hostName = "nixos"; # Define your hostname.
+  networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+
+  time.timeZone = "Europe/Berlin";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "de_DE.UTF-8";
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "de_DE.UTF-8";
+    LC_IDENTIFICATION = "de_DE.UTF-8";
+    LC_MEASUREMENT = "de_DE.UTF-8";
+    LC_MONETARY = "de_DE.UTF-8";
+    LC_NAME = "de_DE.UTF-8";
+    LC_NUMERIC = "de_DE.UTF-8";
+    LC_PAPER = "de_DE.UTF-8";
+    LC_TELEPHONE = "de_DE.UTF-8";
+    LC_TIME = "de_DE.UTF-8";
+  };
+
+  # Configure console keymap
+  console.keyMap = "de";
+
+  security.sudo.wheelNeedsPassword = true;
+
+  # Enable the Flakes feature and the accompanying new nix command-line tool
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  security.polkit.enable = true;
+  networking.firewall.enable = false;
+ 
+  users.users.atlan = {
+    isNormalUser = true;
+    description = "atlan";
+    extraGroups = [ "networkmanager" "wheel" "admin" "dialout" ];
+  };
+
+  programs.zsh.enable = true;
+  users.defaultUserShell = pkgs.zsh;
+
+  home-manager.useGlobalPkgs = true;
+  home-manager.useUserPackages = true;
+  home-manager.users.atlan = {
+    home.stateVersion = "24.05";
+    home.username = "atlan";
+    home.homeDirectory = "/home/atlan";
+  };
+
+ environment.systemPackages = with pkgs; [ 
+    pavucontrol
+    zfs
+    ffmpeg
+    librewolf
+    kate    
+    keepassxc
+    monado-vulkan-layers
+    opencomposite
+    libsurvive
+    xrgears
+  ];
+
+  #Desktop
+  services.xserver.enable = true;
+  services.displayManager.sddm.enable = true;
+  services.xserver.desktopManager.plasma5.enable = true;
+  services.xserver.xkb = {
+    layout = "de";
+    variant = "";
+  };
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  #VR Section
+  programs.steam.enable = true;
+  hardware.steam-hardware.enable = true;
+
+  nixpkgs.overlays = [ 
+    inputs.nixpkgs-xr.overlays.default
+  ];
+  
+  #Enable monado service  
+  services.monado = {
+    enable = true;
+    defaultRuntime = true;
+    highPriority = true;
+  };
+
+  systemd.user.services.monado =
+    {
+      environment = {
+        XRT_COMPOSITOR_COMPUTE = "1";
+        # Use SteamVR tracking (requires calibration with SteamVR)
+        STEAMVR_LH_ENABLE = "true";
+        # Application launch envs:
+        # PRESSURE_VESSEL_FILESYSTEMS_RW=$XDG_RUNTIME_DIR/monado_comp_ipc for Steam applications
+      };
+    };
+
+  #write config for steam
+  home-manager.users.atlan = {
+    xdg.configFile."openxr/1/active_runtime.json".source = "${pkgs.monado}/share/openxr/1/openxr_monado.json";
+
+    xdg.configFile."openvr/openvrpaths.vrpath".text = ''
+      {
+        "config" :
+        [
+          "${config.home-manager.users.atlan.xdg.dataHome}/Steam/config"
+        ],
+        "external_drivers" : null,
+        "jsonid" : "vrpathreg",
+        "log" :
+        [
+          "${config.home-manager.users.atlan.xdg.dataHome}/Steam/logs"
+        ],
+        "runtime" :
+        [
+          "${pkgs.opencomposite-vendored}/lib/opencomposite"
+        ],
+        "version" : 1
+      }
+    '';
+  };
+
+
+  # This option defines the first version of NixOS you have installed on this particular machine,
+  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
+  #
+  # Most users should NEVER change this value after the initial install, for any reason,
+  # even if you've upgraded your system to a new NixOS release.
+  #
+  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
+  # so changing it will NOT upgrade your system.
+  #
+  # This value being lower than the current NixOS release does NOT mean your system is
+  # out of date, out of support, or vulnerable.
+  #
+  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
+  # and migrated your data accordingly.
+  #
+  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
+  system.stateVersion = "23.11"; # Did you read the comment?
+
+}
+
---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "Debug flake vr version";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs-xr.url = "github:nix-community/nixpkgs-xr";
+    nixpkgs-xr.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager = {
+      url = "github:nix-community/home-manager/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs@{ self, nixpkgs, nixpkgs-stable, home-manager, ... }: {
+    nixosConfigurations = {
+      nixos = nixpkgs.lib.nixosSystem rec {
+        system = "x86_64-linux";
+        
+
+        specialArgs = {
+          inherit system inputs;
+          # To use packages from nixpkgs-stable,
+          # we configure some parameters for it first
+          pkgs-stable = import nixpkgs-stable {
+            # Refer to the `system` parameter from
+            # the outer scope recursively
+            inherit system;
+            config.allowUnfree = true;
+          };
+        };
+
+        modules = [
+          home-manager.nixosModules.home-manager {
+            nix.registry.nixos.flake = inputs.self;
+          }
+          ./configuration.nix
+        ];
+      };
+    };
+  };
+}
